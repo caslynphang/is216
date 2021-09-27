5a43310fed939e520bad74ec4ad426fb9f5bf1ff
Darrell Ng <76193629+MosqueBurger@users.noreply.github.com>
2021-09-27 17:11:02 +0800

    mainscript.js added
    This script determines the base of the characteristics of the game, screen size, zoom ratio, bg-colour, physics type etc. 
    
    Look at the comments to find out more

---
+++ mainscript.js
@@ -0,0 +1,35 @@
+// config object that will be passed into phaser.game function
+// if you don't understand any of this just think about it as you customizing your sims character
+// i also don't understand every component
+const config = {
+    width: 512,
+    height: 512,
+    backgroundColour: "#333333",
+    type: Phaser.AUTO,
+    parent: "game-canvas",
+    scene:[],
+    scale: {
+        zoom:2,
+    },
+    //2D games has x and y axis movement so gravity on y-axis is removed, if not the character will fall off screen!
+    physics: {
+        default: 'matter',
+        matter: {
+            debug:true,
+            gravity:{y:0}
+        }
+    },
+    // using phasermattercollisionplugin for eventhandling
+    plugins: {
+        scene:[
+            {
+                plugin: PhaserMatterCollisionPlugin,
+                key: 'matterCollision',
+                mapping: 'matterCollision'
+            }
+        ]
+    }
+}
+
+// creating game object with these attributes
+new Phaser.Game(config);
