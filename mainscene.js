2ad7e8d7df17404021a56630a847879d678bc567
Darrell Ng <76193629+MosqueBurger@users.noreply.github.com>
2021-10-10 22:38:14 +0800

    removed movement, addded buttons!
    added buttons so that our webpage is dynamic, mobile and desktop users will have access, shifted player class to become a file on its own

--- mainscene.js
+++ mainscene.js
@@ -1,4 +1,7 @@
 // MainScene class inherits from Phaser.Scene Class
+import GameButton from "./buttons.js";
+import Player from "./player.js";
+
 export default class MainScene extends Phaser.Scene {
     constructor() {
         // calling scene
@@ -6,47 +9,58 @@ export default class MainScene extends Phaser.Scene {
     }
 
     preload() {
-        console.log("preload");
+        // prelaoding atlas png and json and anim json
+        Player.preload(this);
+        this.load.atlas('buttons', './Assets/Sprites/btn_atlas.png','./Assets/Sprites/btn_atlas.json');
     }
 
 
     create(){
-        console.log("create");
         // assigning player object to a physic
-        this.player = new Phaser.Physics.Matter.Sprite(this.matter.world);
-        // assigning keyboard inputs to variables
-        this.inputKeys = this.input.keyboard.addKeys({
-            up: Phaser.Input.Keyboard.KeyCodes.UP,
-            down: Phaser.Input.Keyboard.KeyCodes.DOWN,
-            left: Phaser.Input.Keyboard.KeyCodes.LEFT,
-            right: Phaser.Input.Keyboard.KeyCodes.RIGHT,
-        })
-    }
+        this.player = new Player({scene:this,x:1000,y:600,texture:'character',frame:'male_1'});
+        // printing sprite onto screen
+        this.player.setScale(3);
+        this.btnUp = this.add.existing(new GameButton(this,1700, 800, 'buttons', 'btnup'));
+        this.btnLeft = this.add.existing(new GameButton(this,1604, 896, 'buttons', 'btnleft'));
+        this.btnDown = this.add.existing(new GameButton(this,1700, 992, 'buttons', 'btndown'));
+        this.btnRight = this.add.existing(new GameButton(this,1796, 896, 'buttons', 'btnright'));
+        this.btnInteract = this.add.existing(new GameButton(this,1700, 896, 'buttons', 'btninteract'));
+        this.btnUp.onPressed=()=> {
+            this.player.setVelocityY(-5);
+            this.player.anims.play('walking_back',true);
+        }
+        this.btnUp.onReleased=()=> {
+            this.player.anims.play('idle_back',true);
+        }
+        this.btnLeft.onPressed =()=> {
+            this.player.setVelocityX(-5);
+            this.player.anims.play('walking_left',true);
+        }
+        this.btnLeft.onReleased=()=> {
+            this.player.anims.play('idle_left',true);
+        }
+        this.btnRight.onPressed =()=> {
+            this.player.setVelocityX(5);
+            this.player.anims.play('walking_right',true);
+        }
+        this.btnRight.onReleased=()=> {
+            this.player.anims.play('idle_right',true);
+        }
+        this.btnDown.onPressed =()=> {
+            this.player.setVelocityY(5);
+            this.player.anims.play('walking_forward',true);
+        }
+        this.btnDown.onReleased=()=> {
+            this.player.anims.play('idle_forward',true);
+        }
 
-    update(){
-        console.log("update");
-        // speed of player, subject to change
-        const speed = 2.5;
-        // assigning velocity to a 2D vector
-        let playerVelocity = new Phaser.Math.Vector2();
-        // assigning keyboard inputs to playerVelocity.x and playerVelocity.y
-        if(this.inputKeys.left.isDown) {
-            playerVelocity.x = -1;
-        }
-        else if (this.inputKeys.right.isDown) {
-            playerVelocity.x = 1;
-        }
-        if(this.inputKeys.up.isDown) {
-            playerVelocity.y = -1;
-        }
-        else if (this.inputKeys.down.isDown) {
-            playerVelocity.y = 1;
-        }
-        // changing 2d Vector movement such that its constant to 1 and only direction matters
-        playerVelocity.normalize();
-        // scaling playerVelocity with speed
-        playerVelocity.scale(speed);
-        // setting player velocity based on input
-        this.player.setVelocity(playerVelocity.x, playerVelocity.y);
+
+    }
+    update() {
+        this.player.update();
+        this.btnUp.update();
+        this.btnLeft.update();
+        this.btnRight.update();
+        this.btnDown.update();
     }
 }
