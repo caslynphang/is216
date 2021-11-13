f717c49dae8535e0a40683f9ffceb6962de93164
zhiwei <89910026+lzwxxx@users.noreply.github.com>
2021-11-14 01:41:09 +0800

    Update login.js

--- login.js
+++ login.js
@@ -34,29 +34,35 @@ function loginUser() {
         passwordError.innerHTML = "Please enter the Password."
     }
 
-    firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
-        
-    }).catch(e => {
-        error.innerHTML = "Incorrect Email Address or Password";
-    })
-
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+        .then(function () {
+            firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
+                window.location.href = "game.html";
+            }).catch(e => {
+                error.innerHTML = "Incorrect Email Address or Password";
+            })
+        })
+        .catch(function (error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+        });
 }
 
 // LOGIN WITH GOOGLE ACCOUNT
 function GoogleLogin() {
-    console.log('Login Btn Call')
-    firebase.auth().signInWithPopup(provider).then(res => {
-        
-    }).catch(e => {
-        console.log(e)
-    })
+    // console.log('Login Btn Call')
+
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+        .then(function () {
+            firebase.auth().signInWithPopup(provider).then(res => {
+                window.location.href = "game.html";
+            }).catch(e => {
+                console.log(e)
+            })
+        })
+        .catch(function (error) {
+            // Handle Errors here.
+            console.log(error);
+        });
 }
-
-// STATE OF THE USER
-firebase.auth().onAuthStateChanged(user => {
-    if (user) { // CHECK IF THE USER IS EXIST 
-        window.location.href = "game.html"; // NEED TO CHANGE TO INDEX.HTML
-    } else { // USER IS SUCCESSFULLY CREATED 
-
-    }
-})
