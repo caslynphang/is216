8e42c056cc5b4dfcba4341aac93b8f05d63ab33a
zhiwei <89910026+lzwxxx@users.noreply.github.com>
2021-11-14 13:20:15 +0800

    Update login.js

--- login.js
+++ login.js
@@ -37,7 +37,7 @@ function loginUser() {
     firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
         .then(function () {
             firebase.auth().signInWithEmailAndPassword(email, password).then(() => {
-                window.location.href = "game.html";
+                // window.location.href = "game.html";
             }).catch(e => {
                 error.innerHTML = "Incorrect Email Address or Password";
             })
@@ -56,7 +56,7 @@ function GoogleLogin() {
     firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
         .then(function () {
             firebase.auth().signInWithPopup(provider).then(res => {
-                window.location.href = "game.html";
+                // window.location.href = "game.html";
             }).catch(e => {
                 console.log(e)
             })
@@ -66,3 +66,11 @@ function GoogleLogin() {
             console.log(error);
         });
 }
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        window.location.href = "game.html";
+    } else {
+        // window.location.href = "index.html";
+    }
+});
