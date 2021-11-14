2f243a30c3df5a26258c97a11b1e12697dc1ea2c
zhiwei <89910026+lzwxxx@users.noreply.github.com>
2021-11-14 18:38:08 +0800

    Update logout.js

--- logout.js
+++ logout.js
@@ -2,7 +2,7 @@ document.getElementById('logoutBtn').addEventListener('click', logoutUser);
 
 function logoutUser() {
     firebase.auth().signOut().then(() => {
-        window.location.href = "index.html";
+        window.location.href = "feedback.html";
     }).catch(e => {
         console.log(e)
     })
